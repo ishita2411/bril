49a523daaa1a13c3970e3ecb712cc7eab8ad87e5
Kundaliya <ishita.kundaliya@hpe.com>
2024-04-19 14:47:50 -0500

    created blocks

--- mycfg/mycfg.py
+++ mycfg/mycfg.py
@@ -5,15 +5,23 @@ def form_blocks(body):
     cur_block = []
 
     for instr in body:
-        if instr['op'] == 'jmp' or instr['op'] == 'ret':
-            cur_block.append(instr)
-            yield
+        if 'op' in instr:
+            if instr['op'] == 'jmp' or instr['op'] == 'ret' or instr['op'] == 'br':
+                cur_block.append(instr)
+                yield cur_block
+                cur_block = []
+            else:
+                cur_block.append(instr)
+        else:
+            yield cur_block
+            cur_block = [instr]
+    yield cur_block
 
 def mycfg():
     prog = json.load(sys.stdin)
-    for fun in prog['func']:
-        form_blocks(func['instrs'])
-
+    for func in prog['functions']:
+        for block in form_blocks(func['instrs']):
+            print(block)
 
 if __name__ == "__main__":
     mycfg()
